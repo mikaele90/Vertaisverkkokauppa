f0c3642cc75cec8577496be1423f46ed86953cb7
Mikael Elgstrom <mikael.elgstrom@metropolia.fi>
2019-05-02 12:49:07 +0300

    index.php päivitetty

--- index.php
+++ index.php
@@ -5,5 +5,12 @@
     <title>Vertaisverkkokauppa</title>
 </head>
 <body>
+<nav>
+    <a href="index.php">Home</a>
+    <a href="registerform.php">Register</a>
+    <a href="login.php">Login</a>
+    <a href="browse.php">Browse</a>
+</nav>
+
 </body>
 </html>
