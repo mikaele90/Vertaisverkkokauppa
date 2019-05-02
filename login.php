df9afe79022aede632d419b3208ac83b6f01ef72
Mikael Elgstrom <mikael.elgstrom@metropolia.fi>
2019-05-02 14:42:02 +0300

    login.php

--- login.php
+++ login.php
@@ -35,7 +35,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT UserId, UserName, UserPassword FROM users WHERE username = ?";
+        $sql = "SELECT UserId, UserName, UserPassword FROM userDB WHERE UserName = ?";
 
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
