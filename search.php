085b3db1d3fce3ef7d72b02eb6e5aff2616d3885
patrikheinonen <patriksh@metropolia.fi>
2019-05-03 16:36:14 +0300

    l

--- search.php
+++ search.php
@@ -45,9 +45,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         } else{
             echo "Oops! Something went wrong. Please try again later.";
         }
-
     }
-
     mysqli_close($link);
 }
 
