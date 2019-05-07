3f32c281802ee8c5843e4e7053bc26bfe22f806c
patrikheinonen <patriksh@metropolia.fi>
2019-05-07 15:24:32 +0300

    l

--- add-credits.php
+++ add-credits.php
@@ -2,15 +2,16 @@
 
 session_start();
 
-$sql = "UPDATE userDB SET Credits = ? WHERE userId = ?";
+$sql = "UPDATE userDB SET Credits = (? + ?) WHERE userId = ?";
 
 if($stmt = mysqli_prepare($link, $sql)){
 
     // Bind variables to the prepared statement as parameters
-    mysqli_stmt_bind_param($stmt, "ii", $param_credits, $param_id);
+    mysqli_stmt_bind_param($stmt, "ii", $param_credits,  $param_id);
 
     // Set parameters
-    $param_credits = null;
+    $param_user_credits = $_SESSION["credits"];
+    $param_credits_to_add = null;
     $param_id = $_SESSION["id"];
 
     if(mysqli_stmt_execute($stmt)){
