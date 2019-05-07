2af9c5f7167a2dcb7d3b6e381ed93e0ef84010fa
Mikael Elgstrom <mikael.elgstrom@metropolia.fi>
2019-05-07 12:47:54 +0300

    profile.php modified

--- profile.php
+++ profile.php
@@ -7,6 +7,37 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
+
+require_once 'database.php';
+
+if (isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] === true) {
+
+    $sql = "SELECT Credits FROM userDB WHERE UserName =  ? ";
+    $credits = null;
+
+    if ($stmt = mysqli_prepare($link, $sql)) {
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
+        //session_start();
+        $param_id = $_SESSION["id"];
+
+        // Attempt to execute the prepared statement
+        if (mysqli_stmt_execute($stmt)) {
+            // Store result
+            mysqli_stmt_store_result($stmt);
+
+            if (mysqli_stmt_num_rows($stmt) == 1) {
+                // Bind result variables
+                mysqli_stmt_bind_result($stmt, $credits);
+
+                if (mysqli_stmt_fetch($stmt)) {
+                    $_SESSION["credits"] = $credits;
+
+                }
+            }
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -22,6 +53,10 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <body>
 
 <?php require_once 'nav-bar.php' ?>
+<?php
+    echo $credits;
+    echo $_SESSION["credits"];
+?>
 
 <div class="page-header">
     <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
